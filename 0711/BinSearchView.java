2f377b3e542dc03f815334410b973689391f7e5c
s17011 <s17011@std.it-college.ac.jp>
2018-07-18 16:23:13 +0900

    Simplified

--- 0711/BinSearchView.java
+++ 0711/BinSearchView.java
@@ -42,28 +42,15 @@ class BinSearchView {
 
 	public static void main(String[] args) {
 		Scanner stdIn = new Scanner(System.in);
-
-		System.out.print("要素数:");
-		int num = stdIn.nextInt();
-		int[] x = new int[num];
-
-		System.out.println("昇順に入力してください。");
-
-		System.out.print("x[0]:");
-		x[0] = stdIn.nextInt();
-
-		for (int i = 1; i < num; i++) {
-			do {
-			System.out.print("x[" + i + "]:");
-			x[i] = stdIn.nextInt();
-			} while (x[i] < x[i - 1]);
-		}
-
+		
+		int[] x = {1,2,3,4,5,6,7,8,9,10};
+		int num = x.length;
+		
 		System.out.print("探す値:");
 		int ky = stdIn.nextInt();
-
+		
 		int idx = binSearchView(x, num, ky);
-
+		
 		if (idx == -1)
 			System.out.println("その値の要素は存在しません。");
 		else
